ab1b8efa369045367b379d423fd14847dfa16527
eleoes <leo.escaroz@gmail.com>
2022-12-10 12:27:55 -0800

    wip: day2

---
+++ day2/day2.py
@@ -0,0 +1,16 @@
+# Get data
+with open('day2.in') as file:
+    data = [i for i in file.read().strip().split('\n')]
+
+# print(data)
+
+x, y, z = 1, 2, 3
+a, b, c, = x, y, z
+
+# Get value of shapes
+
+# Get total score for one round
+    # Get score of shape
+    # Get score of outcome
+
+# Get total score for all rounds
