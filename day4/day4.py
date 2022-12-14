00afe01c41bf792ab96200981a645b7ce42f92f9
eleoes <leo.escaroz@gmail.com>
2022-12-13 17:37:16 -0800

    day4, part 2

--- day4/day4.py
+++ day4/day4.py
@@ -19,3 +19,22 @@ for entry in data:
         counter += 1
 
 print("counter: ", counter)
+
+# Part II
+
+# reset counter
+counter = 0
+
+for entry in data:
+    first, second = entry.split(",")
+
+    first = [int(i) for i in first.split("-")]
+    second = [int(i) for i in second.split("-")]
+
+    if first[0] is range(second[0], second[1]+1) or first[1] in range(second[0], second[1]+1):
+        counter += 1
+
+    elif second[0] is range(first[0], first[1]+1) or second[1] in range(first[0], first[1]+1):
+        counter += 1
+
+print("counter: ", counter)
